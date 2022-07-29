424744e4a16c56674a3d061c38c6e55edaff9b92
Pavel1095 <9153220501@mail.ru>
2022-07-30 01:22:25 +0300

    второе задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // Задание 1
-       int [] arr = new int [3];
-       arr[0] = 1;
-       arr[1] = 2;
-       arr[2] = 3;
-
-       double [] arr2 = {1.57, 7.654, 9.986};
-
-       int [] arr3 = {1, 2, 3, 4, 5};
-
+        System.out.println("Массивы");
+        int[] arrayFirst = new int[3];
+        arrayFirst[0] = 1;
+        arrayFirst[1] = 2;
+        arrayFirst[2] = 3;
+        for (int i = 0; i < arrayFirst.length; i++)
+            if (i == arrayFirst.length - 1) {
+                System.out.print(arrayFirst[i]);
+            } else {
+                System.out.print(arrayFirst[i] + ",");
+            }
+        System.out.println();
+        double[] arraySecond = {1.57, 7.654, 9.986};
+        for (int i = 0; i < arraySecond.length; i++)
+            if (i == arraySecond.length - 1) {
+                System.out.print(arraySecond[i]);
+            } else {
+                System.out.print(arraySecond[i] + ",");
+            }
+        System.out.println();
+        int[] arrayThird = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arrayThird.length; i++)
+            if (i == arrayThird.length - 1) {
+                System.out.print(arrayThird[i]);
+            } else {
+                System.out.print(arrayThird[i] + ",");
+            }
     }
 }
