f069ae625bd76ed98b575ffe4f5d47f00f2e46b6
Pavel1095 <9153220501@mail.ru>
2022-07-29 14:00:22 +0300

    первое задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Задание 1
+       int [] arr = new int [3];
+       arr[0] = 1;
+       arr[1] = 2;
+       arr[2] = 3;
+
+       double [] arr2 = {1.57, 7.654, 9.986};
+
+       int [] arr3 = {1, 2, 3, 4, 5};
 
     }
 }
